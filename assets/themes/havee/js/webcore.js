264bf6a408941611799264c7aad269f24141b80a
Register <458892+aieu@users.noreply.github.com>
2013-12-07 16:29:09 +0800

    add back top

--- assets/themes/havee/js/webcore.js
+++ assets/themes/havee/js/webcore.js
@@ -108,3 +108,22 @@ $(document).ready(function() {
   });
 
 });
+
+
+/* Back Top */
+$("#back-top").hide();
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#back-top').fadeIn();
+    } else {
+      $('#back-top').fadeOut();
+    }
+  });
+  $('#back-top a').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+});
